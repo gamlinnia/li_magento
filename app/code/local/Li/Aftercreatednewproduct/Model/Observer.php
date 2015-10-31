7179503916f0a629aace6d695f8c45a108660d19
thisisbook <gamlinnia@hotmail.com>
2015-10-31 23:29:20 +0800

    log savedProductCustomCallapi

--- app/code/local/Li/Aftercreatednewproduct/Model/Observer.php
+++ app/code/local/Li/Aftercreatednewproduct/Model/Observer.php
@@ -13,11 +13,9 @@ class Li_Aftercreatednewproduct_Model_Observer {
     public function savedProductCustomCallapi (Varien_Event_Observer $observer) {
         Mage::log('run function savedProductCustomCallapi', null, 'customEvent.log');
         /** @var $product Mage_Catalog_Model_Product */
-        $product = clone $observer->getProduct();
-        $event = clone $observer->getEvent();
+        $product = clone $observer->getEvent()->getProduct();
         $originalStore = $product->getStoreId();
         Mage::log($product->debug(), null, 'customEvent.log');
-        Mage::log($event, null, 'customEvent.log');
     }
 
 }
