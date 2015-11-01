18bcc296e5b0c9fe6553fa7c20e64766df598698
thisisbook <gamlinnia@hotmail.com>
2015-11-01 16:23:55 +0800

    schedule indexes

--- app/code/local/Li/Schedulejob/Model/Observer.php
+++ app/code/local/Li/Schedulejob/Model/Observer.php
@@ -18,8 +18,9 @@ class Li_Schedulejob_Model_Observer {
         try {
             /* @var $indexCollection Mage_Index_Model_Resource_Process_Collection */
             $indexCollection = Mage::getModel('index/process')->getCollection();
-            foreach ($indexCollection as $index) {
-                Mage::log('Rebuild indexes:' . $index, null, 'jobs.log');
+            foreach ($indexCollection as $type => $index) {
+                Mage::log('Rebuild indexes:' . $type, null, 'jobs.log');
+                Mage::log($index, null, 'jobs.log');
                 /* @var $index Mage_Index_Model_Process */
                 $index->reindexAll();
             }
