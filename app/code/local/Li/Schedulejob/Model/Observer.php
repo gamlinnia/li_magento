dab8d207ccc042c02ba2c5a83d460f788e95a77b
thisisbook <gamlinnia@hotmail.com>
2015-11-01 15:01:19 +0800

    schedule refresh caches

--- app/code/local/Li/Schedulejob/Model/Observer.php
+++ app/code/local/Li/Schedulejob/Model/Observer.php
@@ -3,7 +3,12 @@
 class Li_Schedulejob_Model_Observer {
 
     public function doSchedules () {
-        Mage::log('do cronjobs', null, 'jobs.log');
+        Mage::log('Refresh caches:', null, 'jobs.log');
+        $allTypes = Mage::app()->useCache();
+        foreach($allTypes as $type => $enabled) {
+            Mage::log('Refresh caches:' . $type, null, 'jobs.log');
+            Mage::app()->getCacheInstance()->cleanType($type);
+        }
     }
 
 }
