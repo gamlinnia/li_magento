d2c2144d8ac469c12afda1232a7f0cf245f759d0
thisisbook <gamlinnia@hotmail.com>
2015-11-01 17:31:22 +0800

    test calling rest

--- app/code/local/Li/Aftercreatednewproduct/Model/Observer.php
+++ app/code/local/Li/Aftercreatednewproduct/Model/Observer.php
@@ -17,6 +17,45 @@ class Li_Aftercreatednewproduct_Model_Observer {
         $originalStore = $product->getStoreId();
         Mage::log($product->debug(), null, 'customEvent.log');
         Mage::log($observer->getEvent()->debug(), null, 'event.log');
+        $restResponse = $this->CallAPI(
+            'GET',
+            'http://rwdev.buyabs.corp/product_sync/rest/route.php/api/getMappedAttributeSetOrSubcategory?inputValue=Speakers&inputType=subCategory',
+            array()
+        );
+        Mage::log($restResponse, null, 'rest.log');
+    }
+
+    public function CallAPI ($method, $url, $header = null, $data = false) {
+        $curl = curl_init();
+
+        switch ($method) {
+            case "POST":
+                curl_setopt($curl, CURLOPT_POST, 1);
+
+                if ($data)
+                    curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+                break;
+            case "PUT":
+                curl_setopt($curl, CURLOPT_PUT, 1);
+                break;
+            default:
+                if ($data)
+                    $url = sprintf("%s?%s", $url, http_build_query($data));
+        }
+
+        /*Custom Header*/
+        if (!empty($header)) {
+            curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
+        }
+
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+        $result = curl_exec($curl);
+
+        curl_close($curl);
+
+        return json_decode($result, true);
     }
 
 }
