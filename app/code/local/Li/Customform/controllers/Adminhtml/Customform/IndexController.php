bac84da5a9661dc1ad2080759e3a67ec79436479
gamlinnia <intppp01>
2015-11-10 14:59:55 +0800

    adminhtml configure custom module admin menu

--- app/code/local/Li/Customform/controllers/Adminhtml/Customform/IndexController.php
+++ app/code/local/Li/Customform/controllers/Adminhtml/Customform/IndexController.php
@@ -3,6 +3,7 @@
 class Li_Customform_Adminhtml_Customform_IndexController extends Mage_Adminhtml_Controller_Action {
 
     public function indexAction () {
+        echo 'customform_index controller';
         $this->loadLayout();
         $this->renderLayout();
     }
