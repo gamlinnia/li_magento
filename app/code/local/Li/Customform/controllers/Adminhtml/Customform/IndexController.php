f257b18de5d66f359d00541460b747f0b5a8e948
gamlinnia <intppp01>
2015-11-10 15:59:26 +0800

    acl of customform admin page

--- app/code/local/Li/Customform/controllers/Adminhtml/Customform/IndexController.php
+++ app/code/local/Li/Customform/controllers/Adminhtml/Customform/IndexController.php
@@ -3,7 +3,6 @@
 class Li_Customform_Adminhtml_Customform_IndexController extends Mage_Adminhtml_Controller_Action {
 
     public function indexAction () {
-        echo 'customform_index controller';
         $this->loadLayout();
         $this->renderLayout();
     }
