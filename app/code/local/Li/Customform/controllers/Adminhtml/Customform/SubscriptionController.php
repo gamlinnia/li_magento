5abcd4166c6dc5fe4419abf2556425604a791a97
thisisbook <gamlinnia@hotmail.com>
2015-11-11 22:32:47 +0800

    _isAllowed

--- app/code/local/Li/Customform/controllers/Adminhtml/Customform/SubscriptionController.php
+++ app/code/local/Li/Customform/controllers/Adminhtml/Customform/SubscriptionController.php
@@ -11,7 +11,7 @@ class Li_Customform_Adminhtml_Customform_SubscriptionController extends Mage_Adm
     }
 
     protected function _isAllowed() {
-        return true;
+        return Mage::getSingleton('admin/session')->isAllowed('system/customform');
     }
 
 }
