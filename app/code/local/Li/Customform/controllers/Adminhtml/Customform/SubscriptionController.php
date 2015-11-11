7f2b671f1d0cfbc67a8ea325fdce1985358bc790
thisisbook <gamlinnia@hotmail.com>
2015-11-11 22:38:44 +0800

    _isAllowed

--- app/code/local/Li/Customform/controllers/Adminhtml/Customform/SubscriptionController.php
+++ app/code/local/Li/Customform/controllers/Adminhtml/Customform/SubscriptionController.php
@@ -10,7 +10,9 @@ class Li_Customform_Adminhtml_Customform_SubscriptionController extends Mage_Adm
         $this->renderLayout();
     }
 
+    /* this method is for acl to grant admin privilege */
     protected function _isAllowed() {
+        /* pass menu tree to isAllowed method */
         return Mage::getSingleton('admin/session')->isAllowed('system/customform');
     }
 
