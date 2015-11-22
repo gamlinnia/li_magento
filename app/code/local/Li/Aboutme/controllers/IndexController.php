88588c42b1ff1cbf6d6abef6486809b0afe9c2e0
thisisbook <gamlinnia@hotmail.com>
2015-11-22 15:16:55 +0800

    about me fom

--- app/code/local/Li/Aboutme/controllers/IndexController.php
+++ app/code/local/Li/Aboutme/controllers/IndexController.php
@@ -10,7 +10,8 @@ class Li_Aboutme_IndexController extends Mage_Core_Controller_Front_Action {
     public function indexAction () {
         $this->loadLayout();
         /*block defined in design_layout*/
-        $this->getLayout()->getBlock('block_aboutme');
+        $this->getLayout()->getBlock('block_aboutme')->setFormAction( Mage::getUrl('*/*/post') );
+        $this->_initLayoutMessages('customer/session');
         $this->renderLayout();
     }
 
