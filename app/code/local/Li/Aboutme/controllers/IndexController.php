b87eb86c8778de2798370014d0c12140b5b302f9
thisisbook <gamlinnia@hotmail.com>
2015-11-22 14:22:14 +0800

    about me fom

--- app/code/local/Li/Aboutme/controllers/IndexController.php
+++ app/code/local/Li/Aboutme/controllers/IndexController.php
@@ -2,11 +2,83 @@
 
 class Li_Aboutme_IndexController extends Mage_Core_Controller_Front_Action {
 
+    const XML_PATH_EMAIL_RECIPIENT  = 'contacts/email/recipient_email';
+    const XML_PATH_EMAIL_SENDER     = 'contacts/email/sender_email_identity';
+    const XML_PATH_EMAIL_TEMPLATE   = 'contacts/email/email_template';
+    const XML_PATH_ENABLED          = 'contacts/contacts/enabled';
+
     public function indexAction () {
         $this->loadLayout();
         /*block defined in design_layout*/
         $this->getLayout()->getBlock('block_aboutme');
         $this->renderLayout();
+    }
+
+    public function postAction () {
+        $post = $this->getRequest()->getPost();
+        Zend_Debug::dump($post);
+
+        if ($post) {
+            $translate = Mage::getSingleton('core/translate');
+            /* @var $translate Mage_Core_Model_Translate */
+            $translate->setTranslateInline(false);
+            try {
+                $postObject = new Varien_Object();
+                $postObject->setData($post);
+
+                $error = false;
+
+                if (!Zend_Validate::is(trim($post['name']) , 'NotEmpty')) {
+                    $error = true;
+                }
+                if (!Zend_Validate::is(trim($post['message']) , 'NotEmpty')) {
+                    $error = true;
+                }
+                if (!Zend_Validate::is(trim($post['email']), 'EmailAddress')) {
+                    $error = true;
+                }
+
+                if ($error) {
+                    throw new Exception();
+                }
+
+                $mailTemplate = Mage::getModel('core/email_template');
+                /* @var $mailTemplate Mage_Core_Model_Email_Template */
+                $mailTemplate->setDesignConfig(array('area' => 'frontend'))
+                    ->setReplyTo($post['email'])
+                    ->sendTransactional(
+                    /*
+                     * default XML_PATH_EMAIL_TEMPLATE => contacts_email_email_template
+                     * default XML_PATH_EMAIL_SENDER => general
+                     * default XML_PATH_EMAIL_RECIPIENT => thisisbook@taiwan-motors.com.tw
+                     * */
+                        1,
+                        Mage::getStoreConfig(self::XML_PATH_EMAIL_SENDER),
+                        Mage::getStoreConfig(self::XML_PATH_EMAIL_RECIPIENT),
+                        null,
+                        array('data' => $postObject)
+                    );
+                if (!$mailTemplate->getSentSuccess()) {
+                    throw new Exception();
+                }
+
+                $translate->setTranslateInline(true);
+
+                Mage::getSingleton('customer/session')->addSuccess(Mage::helper('aboutme')->__('Your inquiry was submitted and will be responded to as soon as possible. Thank you for contacting us.'));
+                $this->_redirect('*/*/');
+
+                return;
+            } catch (Exception $e) {
+                $translate->setTranslateInline(true);
+
+                Mage::getSingleton('customer/session')->addError(Mage::helper('aboutme')->__('Unable to submit your request. Please, try again later'));
+                $this->_redirect('*/*/');
+                return;
+            }
+        } else {
+            $this->_redirect('*/*/');
+        }
+
     }
 
 }
