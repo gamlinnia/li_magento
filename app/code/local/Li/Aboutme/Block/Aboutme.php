42782293a096db3c7af57383b76377559dbb6e9b
thisisbook <gamlinnia@hotmail.com>
2015-12-06 13:28:32 +0800

    resize js

--- app/code/local/Li/Aboutme/Block/Aboutme.php
+++ app/code/local/Li/Aboutme/Block/Aboutme.php
@@ -2,4 +2,8 @@
 
 class Li_Aboutme_Block_Aboutme extends Mage_Core_Block_Template {
 
+    public function getStores () {
+        return Mage::app()->getStores();
+    }
+
 }
--- app/code/local/Li/Aboutme/Helper/Data.php
+++ app/code/local/Li/Aboutme/Helper/Data.php
@@ -1,8 +1,4 @@
 <?php
 class Li_Aboutme_Helper_Data extends Mage_Core_Helper_Abstract {
 
-    public function getStores () {
-        return Mage::app()->getStores();
-    }
-
 }
