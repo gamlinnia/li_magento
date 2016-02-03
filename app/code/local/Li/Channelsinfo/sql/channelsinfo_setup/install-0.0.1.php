69daa4c5d01ff525f77c66791c0ae59dea9a4c74
Li Liu <Li.L.Liu@newegg.com>
2016-02-03 15:00:10 +0800

    channelsinfo module

--- app/code/local/Li/Channelsinfo/sql/channelsinfo_setup/install-0.0.1.php
+++ app/code/local/Li/Channelsinfo/sql/channelsinfo_setup/install-0.0.1.php
@@ -1,51 +1,55 @@
 <?php
 
 $installer = $this;
-/* @var $installer Mage_Catalog_Model_Resource_Setup */
 
 $installer->startSetup();
-
 /**
  * Create table 'catalog/product_attribute_tier_price'
  */
 $table = $installer->getConnection()
-    ->newTable($installer->getTable('catalog/product_attribute_channel_name'))
+    ->newTable($installer->getTable('catalog/product_attribute_channelsinfo'))
     ->addColumn('value_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
         'identity'  => true,
         'nullable'  => false,
-        'primary'   => true,
+        'primary'   => true
     ), 'Value ID')
     ->addColumn('entity_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
         'unsigned'  => true,
-        'nullable'  => false,
-        'default'   => '0',
+        'nullable'  => false
     ), 'Entity ID')
     ->addColumn('channel', Varien_Db_Ddl_Table::TYPE_TEXT, 50, array(
         'nullable'  => false,
-        'default'   => null,
+        'default'   => null
     ), 'Channel')
     ->addColumn('value', Varien_Db_Ddl_Table::TYPE_TEXT, 255, array(
         'nullable'  => false,
-        'default'   => null,
+        'default'   => null
     ), 'Value')
-    ->addIndex(
-        $installer->getIdxName(
-            'catalog/product_attribute_channel_name',
-            array('entity_id', 'channel'),
-            Varien_Db_Adapter_Interface::INDEX_TYPE_UNIQUE
-        ),
-        array('entity_id', 'channel'),
-        array('type' => Varien_Db_Adapter_Interface::INDEX_TYPE_UNIQUE))
-    ->addIndex($installer->getIdxName('catalog/product_attribute_tier_price', array('entity_id')),
-        array('entity_id'))
+    ->addColumn('attribute', Varien_Db_Ddl_Table::TYPE_TEXT, 50, array(
+        'nullable'  => false,
+        'default'   => null
+    ), 'Attribute Name')
     ->addForeignKey(
         $installer->getFkName(
-            'catalog/product_attribute_channel_name',
+            'catalog_product_attribute_channelsinfo',
             'entity_id',
             'catalog/product',
             'entity_id'
         ),
         'entity_id', $installer->getTable('catalog/product'), 'entity_id',
         Varien_Db_Ddl_Table::ACTION_CASCADE, Varien_Db_Ddl_Table::ACTION_CASCADE)
-    ->setComment('Catalog Product Channel Name Attribute Backend Table');
+    ->setComment('Catalog Product Channel Information Attribute Backend Table');
 $installer->getConnection()->createTable($table);
+$installer->endSetup();
+
+$installer->addAttribute('catalog_product', 'channelsinfo', array(
+    'type'                       => 'varchar',
+    'label'                      => 'Channels Information',
+    'input'                      => 'text',
+    'backend'                    => 'catalog/product_attribute_backend_channelsinfo',
+    'required'                   => false,
+    'sort_order'                 => 6,
+    'global'                     => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_WEBSITE,
+    'apply_to'                   => 'simple',
+    'group'                      => 'Channels Information'
+));
