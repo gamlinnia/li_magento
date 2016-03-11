cc5f0ba353083655b268be8eea4ad61e7e39e481
Li Liu <Li.L.Liu@newegg.com>
2016-03-11 16:05:48 +0800

    channelsinfo module

--- app/code/local/Mage/Catalog/Model/Product/Attribute/Backend/Channelsinfo.php
+++ app/code/local/Mage/Catalog/Model/Product/Attribute/Backend/Channelsinfo.php
@@ -6,19 +6,22 @@ class Mage_Catalog_Model_Product_Attribute_Backend_Channelsinfo extends Mage_Eav
     public function afterLoad($object)
     {
         $attrCode = $this->getAttribute()->getAttributeCode();
-        $value = array();
+        $value = array(
+            $attrCode => array('test', 'test123')
+        );
 
         $object->setData($attrCode, $value);
+        var_dump($attrCode);
     }
 
     public function beforeSave($object)
     {
-
+        var_dump('before save');
     }
 
     public function afterSave($object)
     {
-
+        var_dump('after save');
     }
 
 }
