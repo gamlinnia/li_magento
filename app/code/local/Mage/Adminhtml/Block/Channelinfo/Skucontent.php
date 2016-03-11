3e57a38a64686a290f01a2f3a3710203f13e563d
Li Liu <Li.L.Liu@newegg.com>
2016-03-11 09:44:22 +0800

    channelsinfo module

--- app/code/local/Mage/Adminhtml/Block/Channelinfo/Skucontent.php
+++ app/code/local/Mage/Adminhtml/Block/Channelinfo/Skucontent.php
@@ -11,6 +11,22 @@ class Mage_Adminhtml_Block_Channelinfo_Skucontent extends Mage_Adminhtml_Block_W
         $this->setTemplate('channelsinfo/skucontent.phtml');
     }
 
+    protected function _prepareLayout()
+    {
+
+        $this->setChild('add_button',
+            $this->getLayout()->createBlock('adminhtml/widget_button')
+                ->setData(array(
+                    'label' => Mage::helper('catalog')->__('Add New Option'),
+                    'class' => 'add',
+                    'onclick'   => 'answer.add(this)',
+                    'id'    => 'add_new_defined_option'
+                ))
+        );
+
+        return parent::_prepareLayout();
+    }
+
     public function getAddButtonHtml()
     {
         return $this->getChildHtml('add_button');
