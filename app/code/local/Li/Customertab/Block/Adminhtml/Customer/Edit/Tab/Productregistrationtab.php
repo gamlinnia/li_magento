b554711cdb9c41ec048a65b1e7838301deaaa258
Li Liu <Li.L.Liu@newegg.com>
2016-03-15 15:46:41 +0800

    module Li_Customertab

--- app/code/local/Li/Customertab/Block/Adminhtml/Customer/Edit/Tab/Productregistrationtab.php
+++ app/code/local/Li/Customertab/Block/Adminhtml/Customer/Edit/Tab/Productregistrationtab.php
@@ -4,6 +4,7 @@ class Li_Customertab_Block_Adminhtml_Customer_Edit_Tab_Productregistrationtab ex
 {
     public function __construct ()
     {
+        parent::_construct();
         $this->setTemplate('customertab/productregistrationtab.phtml');
     }
 
@@ -24,20 +25,14 @@ class Li_Customertab_Block_Adminhtml_Customer_Edit_Tab_Productregistrationtab ex
         return $this->__('Product Registration');
     }
 
-    public function canShowTab ()
+    public function canShowTab()
     {
-        $customer = Mage::registry('current_customer');
-        return (bool)$customer->getId();
+        return true;
     }
 
     public function isHidden ()
     {
         return false;
-    }
-
-    public function getAfter ()
-    {
-        return 'tags';
     }
 
 }
