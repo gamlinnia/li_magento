b9b2985028b24dfb8fba7aa9c257baca3ce9e431
Li Liu <Li.L.Liu@newegg.com>
2016-04-15 15:28:13 +0800

    module silksoftware_rewritemagento

--- app/code/local/Li/Extendcontroller/controllers/Adminhtml/Mytestcontroller/SubscriptionController.php
+++ app/code/local/Li/Extendcontroller/controllers/Adminhtml/Mytestcontroller/SubscriptionController.php
@@ -1,6 +1,8 @@
 <?php
 
-class Li_Extendcontroller_Adminhtml_Mytestcontroller_SubscriptionController extends Mage_Adminhtml_Controller_Action {
+require_once 'Mage/Adminhtml/controllers/Catalog/ProductController.php';
+class Li_Extendcontroller_Adminhtml_Mytestcontroller_SubscriptionController extends Mage_Adminhtml_Catalog_ProductController
+{
 
     public function indexAction () {
         echo '789';
