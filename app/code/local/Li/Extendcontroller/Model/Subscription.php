c3392c8bc9a3dd9cf4654919325b94e3d4ea22c9
Li Liu <Li.L.Liu@newegg.com>
2016-04-15 15:20:04 +0800

    module silksoftware_rewritemagento

--- app/code/local/Li/Extendcontroller/Model/Subscription.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class Li_Customform_Model_Subscription extends Mage_Core_Model_Abstract {
-    protected function _construct() {
-        $this->_init('customform/subscription');
-    }
-}
---
+++ app/code/local/Li/Extendcontroller/controllers/Adminhtml/Extendcontroller/SubscriptionController.php
@@ -0,0 +1,9 @@
+<?php
+
+class Li_Extendcontroller_Adminhtml_Extendcontroller_SubscriptionController extends Mage_Adminhtml_Controller_Action {
+
+    public function indexAction () {
+        echo '456';
+    }
+
+}
