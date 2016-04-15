06379ed62d9f0b3756fda3ce7443a2e318e75e6d
Li Liu <Li.L.Liu@newegg.com>
2016-04-15 15:24:56 +0800

    module silksoftware_rewritemagento

---
+++ app/code/local/Li/Extendcontroller/controllers/Adminhtml/Mytestcontroller/SubscriptionController.php
@@ -0,0 +1,9 @@
+<?php
+
+class Li_Extendcontroller_Adminhtml_Mytestcontroller_SubscriptionController extends Mage_Adminhtml_Controller_Action {
+
+    public function indexAction () {
+        echo '789';
+    }
+
+}
--- app/code/local/Li/Extendcontroller/controllers/Adminhtml/testController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Li_Extendcontroller_Adminhtml_testController extends Mage_Adminhtml_Controller_Action {
-
-    public function indexAction () {
-        echo '789';
-    }
-
-}
