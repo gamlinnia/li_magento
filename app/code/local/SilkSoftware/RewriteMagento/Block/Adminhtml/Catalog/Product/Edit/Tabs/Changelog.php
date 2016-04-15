937a9d26eeb64f35f9340cb5d9326fc63cc8ba43
Li Liu <Li.L.Liu@newegg.com>
2016-04-15 14:01:08 +0800

    module silksoftware_rewritemagento

---
+++ app/code/local/SilkSoftware/RewriteMagento/Block/Adminhtml/Catalog/Product/Edit/Tabs/Changelog.php
@@ -0,0 +1,17 @@
+<?php
+
+
+class SilkSoftware_RewriteMagento_Block_Adminhtml_Catalog_Product_Edit_Tabs_Changelog extends Mage_Adminhtml_Block_Template
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->setTemplate('rewritemagento/adminhtml/catalog/product/edit/tabs/changelog.phtml');
+    }
+
+
+}
+
+
+?>
