ea685c836d50d4ea031e753741c069b36cf98eb2
Li Liu <Li.L.Liu@newegg.com>
2016-04-15 14:50:04 +0800

    module silksoftware_rewritemagento

--- app/code/local/Solsint/Proextended/controllers/Adminhtml/catalog/ProductController.php
+++ app/code/local/Solsint/Proextended/controllers/Adminhtml/catalog/ProductController.php
@@ -1,6 +1,6 @@
 <?php
 include_once("Mage/Adminhtml/controllers/Catalog/ProductController.php");
-class Solsint_Proextended_Catalog_ProductController extends Mage_Adminhtml_Catalog_ProductController
+class Solsint_Proextended_Adminhtml_Catalog_ProductController extends Mage_Adminhtml_Catalog_ProductController
 {
 
     public function newAction()
