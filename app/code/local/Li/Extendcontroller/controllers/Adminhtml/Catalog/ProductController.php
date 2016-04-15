83974b656c76eff3a06d2fd36020e3b244994722
Li Liu <Li.L.Liu@newegg.com>
2016-04-15 16:16:50 +0800

    module silksoftware_rewritemagento

--- app/code/local/Li/Extendcontroller/controllers/Adminhtml/Catalog/ProductController.php
+++ app/code/local/Li/Extendcontroller/controllers/Adminhtml/Catalog/ProductController.php
@@ -4,10 +4,6 @@ require_once 'Mage/Adminhtml/controllers/Catalog/ProductController.php';
 class Li_Extendcontroller_Adminhtml_Catalog_ProductController extends Mage_Adminhtml_Catalog_ProductController
 {
 
-    public function indexAction () {
-        echo '456';
-    }
-
     public function newAction()
     {
         echo "done"; exit;
